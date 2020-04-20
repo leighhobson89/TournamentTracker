6c0eecc8ba917bad3296ac09ce8bde9a3224e380
Leigh Hobson <leighhobson89@aol.com>
2020-04-20 16:18:52 +0200

    Create text database connection file, read id, write records in new row

--- TournamentTracker/TrackerUI/Program.cs
+++ TournamentTracker/TrackerUI/Program.cs
@@ -19,7 +19,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize the database connections
-            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Textfile);
             Application.Run(new CreatePrizeForm());
             //Application.Run(new TournamentDashboardForm());
         }
